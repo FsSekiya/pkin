cab2ecb1a37affb6b0fe619016d9ff3f18ffa54e
Shengbo.Xu <shengbo.xu@medley.jp>
2017-11-02 01:54:19 +0900

    add: ajax demo

--- app/javascript/packs/components/member-accordion.vue
+++ app/javascript/packs/components/member-accordion.vue
@@ -2,12 +2,16 @@
   <table class="table table-striped table-hover">
     <tbody>
       <tr>
+        <th class="w40">　</th>
         <th>ID</th>
         <th>名前</th>
         <th>所属</th>
         <th>時給</th>
       </tr>
       <tr v-for="col in member_list">
+        <td class="text-center border c-p" v-on:click="sample">
+          <i class="fa fa-plus"></i>
+        </td>
         <td>
           {{ col.id }}
         </td>
@@ -26,6 +30,11 @@
 </template>
 
 <script>
+  import axios from 'axios'
+  var tokens = document.getElementsByName('csrf-token')
+  if (tokens.length) {
+    axios.defaults.headers['X-CSRF-Token'] = tokens.item(0).content
+  }
   export default {
     props: [
       'memberList',
@@ -37,9 +46,22 @@
     },
     created: function () {
       this.member_list = JSON.parse(this.memberList)
+    },
+    methods: {
+      sample: function() {
+        var vm = this
+        axios
+          .get('/api/customer/sample') // dumb IE11 needs encode.
+          .then((res) => {
+            alert("owata")
+          })
+          .catch((res) => {
+            alert("hazimatta")
+          })
+      }
     }
   }
-  </script>
+</script>
 
 <style scoped>
 </style>
