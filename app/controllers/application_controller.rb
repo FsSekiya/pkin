ddc9a9a969ade3bf656db8654192a316ef61abcc
Shengbo.Xu <shengbo.xu@medley.jp>
2017-11-13 01:20:41 +0900

    modify: yarn deploy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,8 +18,8 @@ class ApplicationController < ActionController::Base
   private
 
   def basic_auth
-    authenticate_or_request_with_http_basic do |user,pass|
-      user == ENV["BASIC_AUTH_USER"] && pass == ENV["BASIC_AUTH_PASSWORD"]
+    authenticate_or_request_with_http_basic do |user, pass|
+      user == ENV['BASIC_AUTH_USER'] && pass == ENV['BASIC_AUTH_PASSWORD']
     end
   end
 end
