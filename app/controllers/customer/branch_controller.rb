55691b06293213d4bf3f79fbcadaa4c5007d2260
Yuki Inoue <inoueyuworks@gmail.com>
2017-11-14 21:56:15 +0900

    select workers correctly on branch (#8)

--- app/controllers/customer/branch_controller.rb
+++ app/controllers/customer/branch_controller.rb
@@ -1,6 +1,6 @@
 class Customer::BranchController < Customer::ApplicationController
   def show
     @branch = Branch.find(params[:id])
-    @member_list = Worker.all
+    @member_list = @branch.workers
   end
 end
