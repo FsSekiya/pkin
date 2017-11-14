1ae22637d7c5fc83d11b6ec880fd4dc928842cce
Shengbo.Xu <shengbo.xu@medley.jp>
2017-11-14 23:45:20 +0900

    modify: unscoped

--- app/controllers/customer/record_controller.rb
+++ app/controllers/customer/record_controller.rb
@@ -2,6 +2,6 @@ class Customer::RecordController < Customer::ApplicationController
   def index; end
 
   def show
-    @branch = Branch.find(params[:id])
+    @branch = Branch.unscoped.find(params[:id])
   end
 end
