4528732cb0d0d62e5fae634719936bebfc6e528c
Shengbo.Xu <shengbo.xu@medley.jp>
2017-12-12 20:25:03 +0900

    add: search

--- app/controllers/admin/customer_controller.rb
+++ app/controllers/admin/customer_controller.rb
@@ -1,5 +1,11 @@
 class Admin::CustomerController < Admin::ApplicationController
   layout 'admin'
 
-  def index; end
+  def index
+    @companies = if params[:query].present?
+                   Company.all.where('name like ?', "%#{sanitize_sql_like(params[:query])}%")
+                 else
+                   Company.all
+                 end
+  end
 end
